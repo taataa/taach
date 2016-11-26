2a4c56d12edb01810f33d9e9fc34b26f82579bf4
Akseli Palén <akseli.palen@gmail.com>
2016-11-26 17:02:00 +0200

    Initial mode.view feature

---
+++ examples/view/src/index.js
@@ -0,0 +1,33 @@
+var taach = require('../../../index');
+var taaspace = require('taaspace');
+
+var space = new taaspace.Space();
+var viewElement = document.getElementById('space');
+var view = new taaspace.HTMLSpaceView(space, viewElement);
+
+var taa = new taaspace.Taa('../assets/chellah_star.jpg');
+var staa = new taaspace.SpaceTaa(space, taa);
+
+staa.translate(staa.atMid(), view.atMid());
+
+var touch = new taach.Touchable(view, staa);
+touch.start({ tap: true, scale: true, rotate: true, translate: true });
+
+var viewTouch = new taach.Touchable(view, view);
+viewTouch.element = view._el;  // TODO remove hack
+viewTouch.start({ translate: true, view: true });
+
+(function defineLog() {
+  touch.on('transformstart', function () {
+    console.log('transformstart');
+  });
+  touch.on('transformmove', function () {
+    console.log('transformmove');
+  });
+  touch.on('transformend', function () {
+    console.log('transformend');
+  });
+  touch.on('tap', function () {
+    console.log('tap');
+  });
+}());

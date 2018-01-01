b808ac9e834b6e12009f01d620696fa936ff71a4
Akseli Palén <akseli.palen@gmail.com>
2018-01-01 23:25:18 +0200

    webpack-tape-run test running

--- test/version.test.js
+++ test/version.test.js
@@ -1,16 +1,14 @@
 
-var test = require('tape')
-var semver = require('semver')
-
 var taach = require('../index')
 var pjson = require('../package.json')
+var semver = require('semver')
 
-// Define tests
-
-test('taach.version', function (t) {
-  t.ok(semver.valid(taach.version),
-    'should be correctly formatted')
-  t.equal(taach.version, pjson.version,
-    'should be equal to version in package.json')
-  t.end()
-})
+module.exports = function (test) {
+  test('taach.version', function (t) {
+    t.ok(semver.valid(taach.version),
+      'should be correctly formatted')
+    t.equal(taach.version, pjson.version,
+      'should be equal to version in package.json')
+    t.end()
+  })
+}

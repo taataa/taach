2a0f77cce16e5e9929ccaa0fe6dcb3220c324393
Akseli Palén <akseli.palen@gmail.com>
2018-01-08 00:08:01 +0200

    Rearrange pixels and view

--- examples/modes/index.js
+++ examples/modes/index.js
@@ -7,6 +7,7 @@ var view = new taaspace.SpaceViewHTML(space)
 view.mount(viewElement)
 
 var data = [
+  ['#000000', {}],
   ['#FF0000', { translate: true }],
   ['#00FF00', { scale: true }],
   ['#0000FF', { rotate: true }],
@@ -15,16 +16,20 @@ var data = [
   ['#00FFFF', { scale: true, rotate: true }],
   ['#FFFFFF', { translate: true, scale: true, rotate: true }]
 ]
+var WIDTH = 3
 
 var g = new taaspace.SpaceGroup(space)
 
 data.forEach(function (d, i) {
   var px = new taaspace.SpacePixel(g, d[0])
-  px.translate(px.atMid(), space.at(i, 0))
+  px.translate(px.atMid(), space.at(i % WIDTH, Math.floor(i / WIDTH)))
   var touch = new taach.Touchable(view, px)
   var mode = d[1]
-  mode.pivot = px.atMid()
+  if (!mode.translate) {
+    mode.pivot = px.atMid()
+  }
   touch.start(mode)
 })
 
 view.fit(g.getHull())
+view.scale(view.atMid(), 2)
